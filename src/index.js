ced2e765228b2f2dffd17e28758d29940b7904ab
wrainf <amirfaiz1806@gmail.com>
2022-07-01 13:05:24 +0800

    completed tab switching functionality

--- src/index.js
+++ src/index.js
@@ -7,4 +7,30 @@ import './style.css';
 const content = document.querySelector('#content');
 
 content.appendChild(pageLoad());
-content.appendChild(loadMenu());
+content.appendChild(loadHome());
+
+const home = document.querySelector('#home');
+const menu = document.querySelector('#menu');
+const contact = document.querySelector('#contact');
+
+menu.addEventListener('click', () => {
+    var box = document.querySelector(".content-box");
+    content.removeChild(box);
+    content.appendChild(loadMenu());
+})
+contact.addEventListener('click', () => {
+    var box = document.querySelector(".content-box");
+    content.removeChild(box);
+    content.appendChild(loadContact());
+})
+home.addEventListener('click', () => {
+    var box = document.querySelector(".content-box");
+    content.removeChild(box);
+    content.appendChild(loadHome());
+})
+
+
+
+
+
+
