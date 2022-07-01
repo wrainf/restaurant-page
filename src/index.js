d10bd4532136f65372e9af2c318957c3bfdae08c
wrainf <amirfaiz1806@gmail.com>
2022-07-01 12:24:55 +0800

    called loadMenu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import pageLoad from "./pageLoad";
 import loadHome from "./home";
+import loadMenu from "./menu";
+import loadContact from "./contact";
 import './style.css';
 
 const content = document.querySelector('#content');
 
 content.appendChild(pageLoad());
-content.appendChild(loadHome());
+content.appendChild(loadMenu());
