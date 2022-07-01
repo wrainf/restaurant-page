81220b392a5ef8c24f5ad1a70963042feed1495f
wrainf <amirfaiz1806@gmail.com>
2022-07-01 11:32:55 +0800

    created tab row

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -2,16 +2,35 @@ import Icon from './bg.jpg';
 
 
 function pageLoad(){
-    let element = document.createElement('div');
+    let container = document.createElement('div');
+    container.setAttribute('id','container');
     let welcome = document.createElement('h1');
-    welcome.textContent = "Madam Macaroon";
-    const myImage= new Image();
-    myImage.src = Icon;
-    element.appendChild(welcome);
-    element.appendChild(myImage);
+    welcome.textContent = "Madame Macaroon";
+    
+    const tabRow = document.createElement('div');
+    const homeTab = document.createElement('button');
+    const menuTab = document.createElement('button');
+    const contactTab = document.createElement('button');
+
+    homeTab.setAttribute('id','home');
+    menuTab.setAttribute('id','menu');
+    contactTab.setAttribute('id','contact');
+
+    homeTab.textContent = 'Home';
+    menuTab.textContent = 'Menu';
+    contactTab.textContent = 'Contact';
     
 
-    return element;
+    tabRow.appendChild(homeTab);
+    tabRow.appendChild(menuTab);
+    tabRow.appendChild(contactTab);
+
+    container.appendChild(welcome);
+    container.appendChild(tabRow);
+    
+    
+
+    return container;
 }
 
 export default pageLoad;
