1a4229d6e2b15dcb873319d52c1827cead10f0e9
wrainf <amirfaiz1806@gmail.com>
2022-07-01 22:55:27 +0800

    added add class for when tabs are selected

--- src/index.js
+++ src/index.js
@@ -13,20 +13,32 @@ const home = document.querySelector('#home');
 const menu = document.querySelector('#menu');
 const contact = document.querySelector('#contact');
 
+home.classList.add('selected');
+
 menu.addEventListener('click', () => {
     var box = document.querySelector(".content-box");
     content.removeChild(box);
     content.appendChild(loadMenu());
+    menu.classList.add('selected');
+    home.classList.remove('selected');
+    contact.classList.remove('selected');
+    
 })
 contact.addEventListener('click', () => {
     var box = document.querySelector(".content-box");
     content.removeChild(box);
     content.appendChild(loadContact());
+    contact.classList.add('selected');
+    menu.classList.remove('selected');
+    home.classList.remove('selected');
 })
 home.addEventListener('click', () => {
     var box = document.querySelector(".content-box");
     content.removeChild(box);
     content.appendChild(loadHome());
+    home.classList.add('selected');
+    menu.classList.remove('selected');
+    contact.classList.remove('selected');
 })
 
 
