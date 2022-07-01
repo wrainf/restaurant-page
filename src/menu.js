32a915e24df5f0a6acf5f321e0870c54b18945e4
wrainf <amirfaiz1806@gmail.com>
2022-07-01 12:24:47 +0800

    completed basic loadMenu

---
+++ src/menu.js
@@ -0,0 +1,24 @@
+import Icon from './macaroon.png';
+
+function loadMenu(){
+    let box = document.createElement('div');
+
+    
+
+    for(let i = 0; i < 6; i++){
+        let card = document.createElement('div');
+        const macaroon = new Image();
+        macaroon.src = Icon;
+        let desc = document.createElement('h2');
+        desc.textContent = 'Blueberry';
+        card.appendChild(macaroon);
+        card.appendChild(desc);
+        card.setAttribute('class','card');
+        box.appendChild(card);
+    }
+    
+    box.setAttribute('class','menu');
+    return box;
+}
+
+export default loadMenu;
