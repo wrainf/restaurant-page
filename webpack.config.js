f5f5cf427fb0b59f46b733930f978b7c817ea4d1
wrainf <amirfaiz1806@gmail.com>
2022-07-01 22:54:43 +0800

    added font rule

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,12 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+    
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      
     ],
   },
 };
