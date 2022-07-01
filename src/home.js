7dedb2b7b19eacb53b377df693f8317ba882552d
wrainf <amirfaiz1806@gmail.com>
2022-07-01 11:14:05 +0800

    completed loadHome function

---
+++ src/home.js
@@ -0,0 +1,23 @@
+import Icon from './chef.jpg';
+
+function loadHome(){
+    let box = document.createElement('div');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    p1.textContent = 'Best macaroons in town';
+    p2.textContent = 'Baked with love since 1957';
+    p3.textContent = 'Order online or visit us in store!';
+
+    const chef = new Image();
+    chef.src = Icon;
+
+    box.appendChild(p1);
+    box.appendChild(p2);
+    box.appendChild(chef);
+    box.appendChild(p3);
+
+    return box;
+}
+
+export default loadHome;
